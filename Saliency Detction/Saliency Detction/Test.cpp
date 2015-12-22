08b154f4a1795d2c68c4994ef36de194aee38315
Mojimi <toudsour@gmail.com>
2015-12-22 19:17:54 +0800

    Convert to LAB
    LAB able!

--- Saliency Detction/Saliency Detction/Test.cpp
+++ Saliency Detction/Saliency Detction/Test.cpp
@@ -1,16 +1,19 @@
 #include "HeadFile.h"
 #include "PicPath.h"
 char FileName[MAXPATHLEN];
-
+Mat RGBImage;
+Mat LABImage;
 int main()
 {
 	//GetPic 
 	for(int FileNode=1;FileNode<=MAXPIC;FileNode++)
 	{
 		sprintf(FileName,"%s%d.jpg",IMGSAIM,FileNode);
-		printf("%s\n",FileName);
-		Mat srcImage=imread(FileName);
-		imshow("Pic",srcImage);
+		RGBImage=imread(FileName);
+		imshow("RGB",RGBImage);
+		cvtColor(RGBImage,LABImage,COLOR_BGR2Lab);
+		imshow("LAB",LABImage);
+		waitKey(0);
 	}
 	return 0;
 }
