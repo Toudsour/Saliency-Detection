06c68f8d30f047ef7304ad7a04ff739e49bc0834
Mojimi <toudsour@gmail.com>
2015-12-26 23:03:01 +0800

    Split image into three cannel
    Ohhh.......

--- Saliency Detction/Saliency Detction/HeadFile.h
+++ Saliency Detction/Saliency Detction/HeadFile.h
@@ -2,6 +2,7 @@
 #include<imgproc.hpp>
 #include<iostream>
 #include<String>
+#include<vector>
 #include"spams.h"
 using namespace cv;
 using namespace std;
--- Saliency Detction/Saliency Detction/Test.cpp
+++ Saliency Detction/Saliency Detction/Test.cpp
@@ -3,17 +3,49 @@
 char FileName[MAXPATHLEN];
 Mat RGBImage;
 Mat LABImage;
+void SplitCancle(Mat& SouceImg,Matrix<double>& A,Matrix<double>& B,Matrix<double>& C)
+{
+	int Row=SouceImg.rows;
+	int Col=SouceImg.cols;
+	int Elem=Col*SouceImg.channels();
+	int Num=Row*Col;
+	double *Cannel=new double[Num];
+	cout<<Row<<" "<<Col<<endl;
+	for(int Color=0;Color<3;Color++)
+	{
+		int Count=0;
+		for(int i=0;i<Row;i++)
+		{
+			uchar* Data=SouceImg.ptr<uchar>(i);
+			for(int j=0;j<Elem;j++)
+			{
+				if(j%3==Color)
+					Cannel[Count++]=(double)Data[j]/256;
+			}
+		}
+		switch(Color)
+		{
+			case 0:
+				A.setData(Cannel,Row,Col);
+			case 1:
+				B.setData(Cannel,Row,Col);
+			case 2:
+				C.setData(Cannel,Row,Col);
+		}
+	}
+}
 int main()
 {
 	for(int FileNode=1;FileNode<=MAXPIC;FileNode++)
 	{
-		//GetPic
 		sprintf(FileName,"%s%d.jpg",IMGSAIM,FileNode);
 		RGBImage=imread(FileName);
-		//imshow("RGB",RGBImage);
 		cvtColor(RGBImage,LABImage,COLOR_BGR2Lab);
-		//imshow("LAB",LABImage);
-		
+		imshow("Source",RGBImage);
+		Matrix<double> RGB_R;
+		Matrix<double> RGB_G;
+		Matrix<double> RGB_B;
+		SplitCancle(RGBImage,RGB_R,RGB_G,RGB_B);
 		waitKey(0);
 	}
 	return 0;
